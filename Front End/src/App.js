0619990f6d86d6e715dd8e2fb3ccc2d8984f882d
Sasankavikum <sasavikum@gmail.com>
2023-04-22 02:40:40 +0530

    home page added

--- Front End/src/App.js
+++ Front End/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SideBar from  './components/SideBar'
 
 const App = () => {
-  return <div className = 'overflow-hidden'> Pharmacy web
-  </div>
+  return (
+    <div className='overflow-hidden'>
+      <Router>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <SideBar />
+        <Footer />
+      </Router>
+    </div>
+  )
 }
 
 export default App;
